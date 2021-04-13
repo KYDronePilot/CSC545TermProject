33d6237f72d1232fe2dd500e13b1d60c72dbc96d
KYDronePilot <kydronepilotdev@gmail.com>
2021-04-13 16:08:16 -0400

    Switched to single meal plan instead of weekly/daily ones

---
+++ src/models/MealPlan.java
@@ -0,0 +1,76 @@
+package models;
+
+import database.Database;
+import database.ThrowingConsumer;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import utils.ModelHelper;
+
+public class MealPlan {
+
+    public Integer id;
+    public String name;
+    public String day;
+
+    public MealPlan(Integer id, String name, String day) {
+        this.id = id;
+        this.name = name;
+        this.day = day;
+    }
+
+    public static MealPlan get(Integer id) throws SQLException {
+        return ModelHelper.get(
+            id,
+            "MealPlan",
+            rs -> {
+                return new MealPlan(id, rs.getString("name"), rs.getString("day"));
+            }
+        );
+    }
+
+    public static ArrayList<MealPlan> filter(
+        String sql,
+        ThrowingConsumer<PreparedStatement, SQLException> setValues
+    )
+        throws SQLException {
+        return ModelHelper.filter(
+            sql,
+            setValues,
+            rs -> {
+                return new MealPlan(rs.getInt("id"), rs.getString("name"), rs.getString("day"));
+            }
+        );
+    }
+
+    public static MealPlan create(String name, String day) throws SQLException {
+        var db = Database.getInstance();
+        var id = db.insert(
+            "MealPlan",
+            new String[] { "name", "day" },
+            stmt -> {
+                stmt.setString(1, name);
+                stmt.setString(2, day);
+            },
+            true
+        );
+        return get(id.get());
+    }
+
+    public void update() throws SQLException {
+        var db = Database.getInstance();
+        db.update(
+            "MealPlan",
+            new String[] { "name", "day" },
+            id,
+            stmt -> {
+                stmt.setString(1, name);
+                stmt.setString(2, day);
+            }
+        );
+    }
+
+    public void delete() throws SQLException {
+        ModelHelper.delete(id, "MealPlan");
+    }
+}
