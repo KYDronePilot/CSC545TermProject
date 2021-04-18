1e109cb7520b7ffd818b4d2478618ffadb47d7e6
KYDronePilot <kydronepilotdev@gmail.com>
2021-04-18 17:44:11 -0400

    Formatted table definitions

--- schema.sql
+++ schema.sql
@@ -14,7 +14,8 @@ create table FoodItem (
     foodGroup varchar2(30) not null,
     units number(*, 0) default 0 not null,
     nutritionFactsId number not null,
-    constraint nutritionFactsFk foreign key (nutritionFactsId) references NutritionFacts(id) on delete cascade
+    constraint nutritionFactsFk foreign key (nutritionFactsId)
+        references NutritionFacts(id) on delete cascade
 );
 
 create table Recipe (
@@ -28,21 +29,26 @@ create table RecipeFoodItem (
     recipeId number,
     foodItemId number,
     primary key (recipeId, foodItemId),
-    constraint foodItemRecipeFk foreign key (recipeId) references Recipe(id) on delete cascade,
-    constraint recipeFoodItemFk foreign key (foodItemId) references FoodItem(id) on delete cascade
+    constraint foodItemRecipeFk foreign key (recipeId)
+        references Recipe(id) on delete cascade,
+    constraint recipeFoodItemFk foreign key (foodItemId)
+        references FoodItem(id) on delete cascade
 );
 
 create table MealPlan (
     id number generated always as identity primary key,
     name varchar2(20) not null,
     day char(3) not null unique,
-    constraint validateDayOfWeekCheck check (day in ('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'))
+    constraint validateDayOfWeekCheck
+        check (day in ('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'))
 );
 
 create table RecipeMealPlan (
     recipeId number,
     mealPlanId number,
     primary key (recipeId, mealPlanId),
-    constraint recipeFk foreign key (recipeId) references Recipe(id) on delete cascade,
-    constraint mealPlanFk foreign key (mealPlanId) references MealPlan(id) on delete cascade
+    constraint recipeFk foreign key (recipeId)
+        references Recipe(id) on delete cascade,
+    constraint mealPlanFk foreign key (mealPlanId)
+        references MealPlan(id) on delete cascade
 );
